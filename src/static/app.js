640350f894fbdbcbb9c482651c4ed58332efc89e
satak <sami.koskivaara@gmail.com>
2019-03-01 14:53:13 +0200

    Fix JS

--- src/static/app.js
+++ src/static/app.js
@@ -1,34 +1,10 @@
-function getKey() {
-  const body = {
-    action: 'get_key'
-  }
-  const requestParams = {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(body)
-  }
+async function API(action, key, inputData) {
   const url = '/cryptography'
-  fetch(url, requestParams)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (myJson) {
-      $("#key").val(myJson.data)
-    });
-}
-
-function cryptography(action) {
-  const key = $("#key").val()
-  const inputData = $("#data").val()
   const body = {
     action: action,
     key: key,
     data: inputData
   }
-  const url = '/cryptography'
   const requestParams = {
     method: 'POST',
     headers: {
@@ -38,11 +14,23 @@ function cryptography(action) {
     body: JSON.stringify(body)
   }
 
-  fetch(url, requestParams)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (myJson) {
-      $("#data").val(myJson.data)
-    });
+  const response = await fetch(url, requestParams)
+  if (response.ok) return await response.json()
+  throw new Error(await response.text());
+}
+
+function getKey() {
+  API('get_key').then(response => $("#key").val(response.data)).catch(alert)
+}
+
+function cryptography(action) {
+  const key = $("#key").val()
+  const inputData = $("#data").val()
+  API(action, key, inputData).then(response => $("#data").val(response.data)).catch(alert)
+}
+
+function clearAll() {
+  $("#key").val('')
+  $("#data").val('')
+  console.log('clear')
 }
